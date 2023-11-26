fedfe0f4de409d71d24347a7fdac926ece119523
VladDev322 <VladL322@yandex.ru>
2023-11-27 02:47:34 +0300

    feat: section category

--- js/swiper.js
+++ js/swiper.js
@@ -1,27 +1,49 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
+const swiper = new Swiper('.swiper-1', {
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination-1',
+    clickable: true,
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  autoplay: {
+    delay: 3000,
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-
-  // autoplay: {
-  //   delay: 3000,
-  // },
 });
+
+const swiper2 = new Swiper('.swiper-2', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next-2',
+    prevEl: '.swiper-button-prev-2',
+  },
+
+  slidesPerView: 4,
+  spaceBetween: 10,
+
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+// const swiper3 = new Swiper('.swiper-3', {
+//   direction: 'horizontal',
+//   loop: true,
+
+//   navigation: {
+//     nextEl: '.swiper-button-next-3',
+//     prevEl: '.swiper-button-prev-3',
+//   },
+
+//   slidesPerView: 4,
+//   spaceBetween: 10,
+
+//   autoplay: {
+//     delay: 3000,
+//   },
+// });
